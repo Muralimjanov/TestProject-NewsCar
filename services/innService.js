5b3b194ccc7b8939c43079ebc02fc8e7f1cd01f9
Muralimjanov <muralimjanov@mail.ru>
2025-05-15 02:54:01 +0600

    inn key

--- services/innService.js
+++ services/innService.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
+import config from 'config';
 
 export const fetchCompanyInfoByInn = async (inn) => {
     try {
-        const response = await axios.get(`https://api-fns.ru/api/egr?req=${inn}`);
-        const {data} = response;
+        const response = await axios.get(`https://api-fns.ru/api/egr`, {
+            params: {
+                req: inn,
+                key: config.get("API_KEY_INN"),
+            },
+        });
+
+        const { data } = response;
 
         if (data && data.items && data.items.length > 0) {
             return {
@@ -17,6 +24,7 @@ export const fetchCompanyInfoByInn = async (inn) => {
             };
         }
     } catch (error) {
+        console.error('ИНН API error:', error?.response?.data || error.message);
         return {
             success: false,
             message: 'Ошибка при проверке ИНН',
