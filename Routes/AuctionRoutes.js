c3b1394b2440ea63c9f6eaf5bcd06c7a89cdeb9a
Artur Novikov <artur.20177102@mail.ru>
2025-06-13 20:42:16 +0300

    removed user verification in receiving auctions

--- Routes/AuctionRoutes.js
+++ Routes/AuctionRoutes.js
@@ -1,30 +1,30 @@
-import { Router } from 'express';
-import { validateBody } from '../Middleware/validateBody.js';
-import { auctionSchema } from '../Validations/AuctionValidation.js';
+import { Router } from "express";
+import { validateBody } from "../Middleware/validateBody.js";
+import { auctionSchema } from "../Validations/AuctionValidation.js";
 import {
-    createAuction,
-    getAuctionById,
-    getAllAuctions,
-    updateAuction,
-    deleteAuction,
-    deleteManyAuctions,
-    createManyAuctions,
-    updateManyAuctions,
-} from '../Controllers/AuctionControllers.js';
-import { checkAuth } from '../Middleware/authMiddleware.js';
-import { checkRole } from '../Middleware/checkRole.js';
+  createAuction,
+  getAuctionById,
+  getAllAuctions,
+  updateAuction,
+  deleteAuction,
+  deleteManyAuctions,
+  createManyAuctions,
+  updateManyAuctions,
+} from "../Controllers/AuctionControllers.js";
+import { checkAuth } from "../Middleware/authMiddleware.js";
+import { checkRole } from "../Middleware/checkRole.js";
 
 const router = Router();
 
-router.use(checkAuth, checkRole('admin'));
+router.use(checkAuth, checkRole("admin"));
 
-router.get('/', checkAuth, getAllAuctions);
-router.post('/', validateBody(auctionSchema), createAuction);
-router.post('/bulk', createManyAuctions);
-router.put('/:id', validateBody(auctionSchema), updateAuction);
-router.put('/bulk', updateManyAuctions);
-router.get('/:id', getAuctionById);
-router.delete('/:id', deleteAuction);
-router.delete('/bulk', deleteManyAuctions);
+router.get("/", getAllAuctions);
+router.post("/", validateBody(auctionSchema), createAuction);
+router.post("/bulk", createManyAuctions);
+router.put("/:id", validateBody(auctionSchema), updateAuction);
+router.put("/bulk", updateManyAuctions);
+router.get("/:id", getAuctionById);
+router.delete("/:id", deleteAuction);
+router.delete("/bulk", deleteManyAuctions);
 
 export default router;
